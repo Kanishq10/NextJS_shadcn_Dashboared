22bd485014ceb19e0a88729344cc1822270eeaf0
Kanishq <79330066+Kanishq10@users.noreply.github.com>
2025-11-29 20:46:45 +0530

    Some changes

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -8,8 +8,15 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
+
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import Link from "next/link";
+import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
 
 const SingleUserPage = () => {
     return (
@@ -51,8 +58,82 @@ const SingleUserPage = () => {
             <div className="mt-4 flex flex-col xl:flex-row gap-8">
                 {/* LEFT */}
                 <div className="w-full xl:w-1/3 space-y-6">
-                    <div className="bg-primary-foreground p-4 rounded-lg">Badge</div>
-                    <div className="bg-primary-foreground p-4 rounded-lg">Info</div>
+                    {/*User Badge  */}
+                    <div className="bg-primary-foreground p-4 rounded-lg">
+                        <h1 className="text-xl font-semibold">User Badges</h1>
+                        <div className="flex gap-4 mt-4">
+                            <HoverCard>
+                                <HoverCardTrigger>
+                                    <BadgeCheck
+                                        size={36}
+                                        className="rounded-full bg-blue-500/30 border border-blue-500/50 p-2"
+                                    />
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <h1 className="font-bold mb-2">Verified User</h1>
+                                    <p className="text-sm text-muted-foreground">
+                                        This user has been verified by the admin.
+                                    </p>
+                                </HoverCardContent>
+                            </HoverCard>
+                            <HoverCard>
+                                <HoverCardTrigger>
+                                    <Shield
+                                        size={36}
+                                        className="rounded-full bg-green-800/30 border border-green-800/50 p-2"
+                                    />
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <h1 className="font-bold mb-2">Admin</h1>
+                                    <p className="text-sm text-muted-foreground">
+                                        Admin users have access to all features and can manage
+                                        users.
+                                    </p>
+                                </HoverCardContent>
+                            </HoverCard>
+                            <HoverCard>
+                                <HoverCardTrigger>
+                                    <Candy
+                                        size={36}
+                                        className="rounded-full bg-yellow-500/30 border border-yellow-500/50 p-2"
+                                    />
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <h1 className="font-bold mb-2">Awarded</h1>
+                                    <p className="text-sm text-muted-foreground">
+                                        This user has been awarded for their contributions.
+                                    </p>
+                                </HoverCardContent>
+                            </HoverCard>
+                            <HoverCard>
+                                <HoverCardTrigger>
+                                    <Citrus
+                                        size={36}
+                                        className="rounded-full bg-orange-500/30 border border-orange-500/50 p-2"
+                                    />
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <h1 className="font-bold mb-2">Popular</h1>
+                                    <p className="text-sm text-muted-foreground">
+                                        This user has been popular in the community.
+                                    </p>
+                                </HoverCardContent>
+                            </HoverCard>
+                        </div>
+                    </div>
+                    <div className="bg-primary-foreground p-4 rounded-lg">
+                        <h1 className="text-xl font-semibold">User Information</h1>
+                        <div className="space-y-4 mt-4">
+                            <div className="flex flex-col gap-2 mb-8">
+                                <p className="text-sm text-muted-foreground">Profile completion</p>
+                                <progress value={66} />
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <span className="font-bold">Username: </span>
+                            <span>Jhon_Depp</span>
+                        </div>
+                    </div>
                     <CardList title="Recent Transactions" />
                 </div>
                 {/* RIGHT */}
